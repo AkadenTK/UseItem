b2a961cf4c30a5e345354da7c7a39e8d3bcec59a
AkadenTK <harms.blake@gmail.com>
2023-01-09 16:01:58 -0600

    Update items.lua

--- items.lua
+++ items.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright © 2018, Akaden
+Copyright © 2023, Akaden
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
